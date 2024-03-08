3f6ece01ef682a70c4bc2d8a4f3bcab32043dc0f
Semen Shneider <shneydik.sam@gmail.com>
2024-03-08 21:09:25 +0100

    initial commit

---
+++ Homework_2/Task_1.py
@@ -0,0 +1,4 @@
+k1 = float(input("Enter the length of the first k1: "))
+k2 = float(input("Enter the length of the second k2: "))
+
+print("Square of the triangle is: ", 1/2*k1*k2)
---
+++ Homework_2/Task_3.py
@@ -0,0 +1,5 @@
+num = int(input("Введите число: "))
+Next_num = num + 1
+Previous_num = num - 1
+print("Cледующее число", Next_num)
+print("Предыдущее число", Previous_num)
