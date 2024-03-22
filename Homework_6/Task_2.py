2a53d7a100e27d1eb0906b134722c5b5945adf71
Semen Shneider <37775931+senya2006@users.noreply.github.com>
2024-03-22 20:51:05 +0100

    Revert "For homework"

--- Homework_6/Task_2.py
+++
@@ -1,8 +0,0 @@
-my_list = [1, 5, 68, 0]
-# my_list = [1,2,3,4,6,7,8]
-for i in range(1, len(my_list)):
-    if my_list[i] != my_list[i-1] + 1:
-        print("Non-consecutive number in the list: ", my_list[i])
-        break
-else:
-    print("The list contains consecutive numbers!")
