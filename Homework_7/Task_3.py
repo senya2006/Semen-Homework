70c90a6ed7fa794a3531d00e30a6c58b7799fb36
Semen Shneider <shneydik.sam@gmail.com>
2024-03-27 17:14:58 +0100

    fixed 1 and 3 tasks

--- Homework_7/Task_3.py
+++ Homework_7/Task_3.py
@@ -1,6 +1,5 @@
 numbers = [1, 2, 3, 4, 5, 6, 2, 7, 8, 9, 11]
-unique_numbers = set(numbers)
-if len(unique_numbers) == len(numbers):
+if len(numbers) == len(set(numbers)):
     print(f"This set: {numbers} are unique")
 else:
     print(f"This set: {numbers} have similar numbers")
