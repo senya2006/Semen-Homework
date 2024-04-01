b6135b16bdda6192d9370626fccd3f55a8c6cd82
Semen Shneider <shneydik.sam@gmail.com>
2024-04-01 18:50:40 +0200

    fixed mistakes after review

--- Homework_8/Task_3.py
+++ Homework_8/Task_3.py
@@ -1,9 +1,9 @@
 result_link = 'https://www.youtube.com/watch?v=some_id&list=some_list&index=6&t=0s'
 dict_count = {}
 
-for dict in result_link:
-    if dict in dict_count:
-        dict_count[dict] += 1
+for dict1 in result_link:
+    if dict1 in dict_count:
+        dict_count[dict1] += 1
     else:
-        dict_count[dict] = 1
+        dict_count[dict1] = 1
 print(f'How many times an element is repeated in a line: {dict_count}')
