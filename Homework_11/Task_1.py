e21e42ae435166b803fbf9d2c9e0056d8da1548a
Semen Shneider <shneydik.sam@gmail.com>
2024-04-11 16:57:32 +0200

    fixed mistakes after review

--- Homework_11/Task_1.py
+++ Homework_11/Task_1.py
@@ -5,18 +5,15 @@ def fahrenheit_to_celsius(fahrenheit: float):
     return (fahrenheit - 32) * 5/9
 
 def convert_temperature(temperature: float, unit: str):
-    try:
-        if unit.upper() == 'C':
-            return celsius_to_fahrenheit(temperature)
-        elif unit.upper() == 'F':
-            return fahrenheit_to_celsius(temperature)
-        else:
-            raise ValueError("Error. Please use 'C' for Celsius or 'F' for Fahrenheit.")
-    except ValueError as e:
-        return str(e)
+    if unit.upper() == 'C':
+        return celsius_to_fahrenheit(temperature)
+    elif unit.upper() == 'F':
+        return fahrenheit_to_celsius(temperature)
+    else:
+        raise ValueError("Error. Please use 'C' for Celsius or 'F' for Fahrenheit.")
 
 temperature = float(input("Enter the temperature: "))
 unit = input("Enter the unit of measurement (C for Celsius, F for Fahrenheit): ")
 
 converted_temperature = convert_temperature(temperature, unit)
-print(f"The converted temperature from temperature: {temperature} and unit: {unit} is: {converted_temperature}")
+print(f"The converted temperature from temperature: {temperature} and unit: {unit.upper()} is: {converted_temperature}")
