8eade8df02bde1ce57101fbbcc49859c5b748345
Semen Shneider <shneydik.sam@gmail.com>
2024-04-21 14:04:17 +0200

    fixed after review

--- Homework_13/Task_1.py
+++ Homework_13/Task_1.py
@@ -2,7 +2,10 @@ class Worker:
     def __init__(self, name, position, salary):
         self._name = name
         self.position = position
-        self.salary = salary
+        self.__salary = salary
+
+    def __str__(self):
+        return f'Worker: {self.name}, Position: {self.position}, Salary: ${self.__salary}'
 
     @property
     def name(self):
@@ -14,12 +17,8 @@ class Worker:
             raise ValueError("Name must be a string")
         self._name = value
 
-    def __str__(self):
-        return f'Worker: {self.name}, Position: {self.position}, Salary: ${self.salary}'
-
-# Example usage:
-worker1 = Worker("Vasya", "Cleaner", 300)
-print(worker1)
-
-worker1.name = "Petya"
-print(worker1)
+if __name__ == '__main__':
+    worker1 = Worker("Vasya", "Cleaner", 300)
+    print(worker1)
+    worker1.name = "Petya"
+    print(worker1)
